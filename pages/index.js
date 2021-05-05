a268ec54eb02629bc3257fe3887475560df5027f
alvinuche <valvin900@gmail.com>
2021-05-05 02:29:38 +0100

    fetch increase fetch page_size

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export const getStaticProps = async () => {
 	};
 
 	const res = await fetch(
-		`https://newscatcher.p.rapidapi.com/v1/search_free?q=cryptocurrency&lang=en&page=50&page_size=12&media=True`,
+		`https://newscatcher.p.rapidapi.com/v1/search_free?q=cryptocurrency&lang=en&page=5&page_size=15&media=True`,
 		{
 			method: "GET",
 			headers: {
